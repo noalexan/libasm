96465f75a3ef0302f75b01e6f886a0f37e49a717
noalexan <noalexan@student.42nice.fr>
2024-08-10 00:04:31 +0200

    Add `ft_strcmp`

--- libasm.h
+++ libasm.h
@@ -4,3 +4,4 @@
 
 size_t ft_strlen(const char *);
 char   *ft_strcpy(char *dest, const char *src);
+int    ft_strcmp(const char *str1, const char *str2);
---
+++ src/ft_strcmp.s
@@ -0,0 +1,29 @@
+global ft_strcmp
+
+ft_strcmp:
+    push rbp
+    mov rbp,rsp
+
+    xor rax, rax
+    xor rbx, rbx
+
+.loop:
+    mov al, byte [rdi]
+    mov bl, byte [rsi]
+
+    sub rax, rbx
+    jnz .end
+
+    test bl, bl
+    jz .end
+
+    inc rdi
+    inc rsi
+
+    jmp .loop
+
+.end:
+    mov rsp,rbp
+    pop rbp
+
+    ret
